d9dfc8776320c26412fa4bb753632be21787f473
misstaqe <jsonfreel@gmail.com>
2021-06-11 02:27:32 +1000

    Moved duration() to all.js

--- all.js
+++ all.js
@@ -1,30 +1,49 @@
-function timeSince (date) {
+function timeSince(date) {
     var seconds = Math.floor((new Date() - date * 1000) / 1000);
     var interval = seconds / 31536000;
 
     if (interval > 1) {
-    return Math.floor(interval) + " years ago";
+        return Math.floor(interval) + " years ago";
     }
 
     interval = seconds / 2592000;
     if (interval > 1) {
-    return Math.floor(interval) + " months ago";
+        return Math.floor(interval) + " months ago";
     }
 
     interval = seconds / 86400;
     if (interval > 1) {
-    return Math.floor(interval) + " days ago";
+        return Math.floor(interval) + " days ago";
     }
 
     interval = seconds / 3600;
     if (interval > 1) {
-    return Math.floor(interval) + " hours ago";
+        return Math.floor(interval) + " hours ago";
     }
 
     interval = seconds / 60;
     if (interval > 1) {
-    return Math.floor(interval) + " minutes ago";
+        return Math.floor(interval) + " minutes ago";
     }
     
     return Math.floor(seconds) + " seconds ago";
+}
+
+function duration(t0, t1) {
+    let d = (new Date(t1)) - (new Date(t0));
+    let weekdays = Math.floor(d / 1000 / 60 / 60 / 24 / 7);
+    let days = Math.floor(d / 1000 / 60 / 60 / 24 - weekdays * 7);
+    let hours = Math.floor(d / 1000 / 60 / 60 - weekdays * 7 * 24 - days * 24);
+    let minutes = Math.floor(d / 1000 / 60 - weekdays * 7 * 24 * 60 - days * 24 * 60 - hours * 60);
+    let seconds = Math.floor(d / 1000 - weekdays * 7 * 24 * 60 * 60 - days * 24 * 60 * 60 - hours * 60 * 60 - minutes * 60);
+    let milliseconds = Math.floor(d - weekdays * 7 * 24 * 60 * 60 * 1000 - days * 24 * 60 * 60 * 1000 - hours * 60 * 60 * 1000 - minutes * 60 * 1000 - seconds * 1000);
+    let t = {};
+
+    ['weekdays', 'days', 'hours', 'minutes', 'seconds', 'milliseconds'].forEach(q => { 
+        if (eval(q) > 0) { 
+            t[q] = eval(q);
+        } 
+    });
+
+    return t;
 }
